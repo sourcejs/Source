a09ed5590099b3dfc4f51324e1635d8479a3c430
Robert Haritonov <r@rhr.me>
2013-06-03 11:34:31 +0300

    Fixing some merge conflicts with v.2.0.0

--- core/js/modules/globalNav.js
+++ core/js/modules/globalNav.js
@@ -169,7 +169,7 @@ define([
                     var addNavPosition = function (target) {
 
                         if (target.author != '') {
-                            authorName = ' | Автор: ' + target.author + '';
+                            authorName = ' | ' + RES_AUTHOR + ': ' + target.author + '';
                         } else {
                             authorName = '';
                         }
