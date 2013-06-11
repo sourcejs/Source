e76764d8c63f232df4886642aba4dd805dab6e66
jurispukitis <jurispukitis20@gmail.com>
2013-06-11 17:18:45 +0300

    index.json info parsing to pages_tree.json

--- plugins/file_tree_generator/core/file_tree_generator.php
+++ plugins/file_tree_generator/core/file_tree_generator.php
@@ -106,7 +106,7 @@ function getFilesTree($dir, $includeFilesMask = "/.*/", $scanDirMask = "/.*/", $
 					$DIR_NAME = dirname($URL);
 
 					$BUBBLE_DATA_PATH = $DOCUMENT_ROOT.$DIR_NAME.'/temp/bubbles.dat';
-					$PROJECT_DATA_PATH = $DOCUMENT_ROOT.$DIR_NAME.'/data.json';
+					$PROJECT_DATA_PATH = $DOCUMENT_ROOT.$DIR_NAME.'/index.json';
 
 					if (file_exists($PROJECT_DATA_PATH)) {
 						$json = file_get_contents($PROJECT_DATA_PATH);
@@ -120,7 +120,7 @@ function getFilesTree($dir, $includeFilesMask = "/.*/", $scanDirMask = "/.*/", $
 						"lastmodSec" => $lastmodSec,
 						"keywords" => isset($keyword[1]) ? $keyword[1] : "",
 						"bubbles" => GET_BUBBLES_COUNT($BUBBLE_DATA_PATH),
-						"projectData" => isset($data) ? $data : "",
+						"info" => isset($data) ? $data : "",
 					);
 				}
 			}
