f9b1acab16b4082fffb5c0beae901ff1fe131acd
ivan.metelev <ivan.metelev@odnoklassniki.ru>
2014-04-07 16:06:25 +0400

    events plugin

--- client-side/src/core/js/modules/scrollToHash.js
+++ client-side/src/core/js/modules/scrollToHash.js
@@ -1,13 +1,13 @@
 //In separate module, to trace script ready state
 define([
     "modules/utils",
+    "modules/loadEvents",
     "modules/sectionFolding"
-    ], function(utils) {
+    ], function(utils, loadEvents) {
     var navHash = utils.parseNavHash(),
 	    sections = document.querySelectorAll('.source_section'),
 	    navigation = document.querySelector('.source_nav'),
-	    mainContainer = document.querySelector('.source_main'),
-    	defaultTimeOut = 1000;
+	    mainContainer = document.querySelector('.source_main');
 
 	// Show hidden sections and navigation
 	function showSections() {
@@ -19,77 +19,25 @@ define([
 		mainContainer.className = mainContainer.className.replace(' __loading', '');
 	}
 
-	// If any navigation hash exists, let's scroll
-    if (navHash != '') {
+	mainContainer.className += ' __loading';
 
-		mainContainer.className += ' __loading';
+	loadEvents.init(function() {
+		showSections();
 
-    	(function checkForScroll() {
-    		// Any different render plugins can be used,
-    		// so we need variables for count all registered plugins and
-    		// all loaded plugins
+		// Chrome scroll bug
+		if ( document.querySelector('html').className.indexOf('webkit') != '-1' ) {
 
-    		var pluginCount = 0,
-    			pluginReady = 0;
-
-			// If there's no any render plugin, just skip checking section
-			if (window.render !== undefined) {
-
-				for (var plugin in window.render) {
-					pluginCount++;
-
-					// first time
-					if (window.render[plugin].timeExpected === undefined) {
-						window.render[plugin].timeExpected = new Date().getTime() + (window.render[plugin].timeOut || defaultTimeOut);
-
-						// When plugin finish his work, it send "finishEvent", which differs for each plugin
-						window.render[plugin].finishEvent && document.addEventListener(window.render[plugin].finishEvent, function() {
-							window.render[plugin].timeExpected = new Date().getTime();
-						})
-
-						// When plugin need to update work's timeout, it generate "updateEvent"
-						window.render[plugin].updateEvent && document.addEventListener(window.render[plugin].updateEvent, function() {
-							window.render[plugin].timeExpected = new Date().getTime() + (window.render[plugin].timeOut || defaultTimeOut);
-						})
-
-					// Plugin already was initialized
-					} else {
-						// If timeout come, increase counter
-						if ( window.render[plugin].timeExpected < new Date().getTime() ) {
-							pluginReady++;
-						}
-					}
+			var t = setInterval(function() {
+				if (window.pageYOffset != 0) {
+					clearInterval(t);
 				}
 
-				// if there's more than 0 plugins and all of them get timeout, continue,
-				// otherwise make recursive call
-				if ( !((pluginCount) && (pluginCount == pluginReady)) ) {
-					setTimeout(function() {
-						checkForScroll();
-					}, 100);
-					return;
-				}
-			}
-
-			showSections();
-
-			// Chrome scroll bug
-			if ( document.querySelector('html').className.indexOf('webkit') != '-1' ) {
-
-				var t = setInterval(function() {
-					if (window.pageYOffset != 0) {
-						clearInterval(t);
-					}
-
-					window.scrollTo(0, 1);
-					utils.scrollToSection(navHash);
-				}, 100)
-			} else {
+				window.scrollTo(0, 1);
 				utils.scrollToSection(navHash);
-			}
-    	})();
+			}, 100)
+		} else {
+			utils.scrollToSection(navHash);
+		}
+	});
 
-    } else {
-    	showSections();
-    }
 });
