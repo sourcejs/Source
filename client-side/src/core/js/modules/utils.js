591b54f0ca18a7daeae15da47ac02f00174af0fa
ivan.metelev <ivan.metelev@odnoklassniki.ru>
2014-04-08 19:26:30 +0400

    event plugin update

--- client-side/src/core/js/modules/utils.js
+++ client-side/src/core/js/modules/utils.js
@@ -73,5 +73,20 @@ define([
         return false;
     };
 
+    Utils.prototype.isDevelopmentMode = function() {
+
+		function getCookie(name) {
+			var matches = document.cookie.match(new RegExp("(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"));
+			return matches ? decodeURIComponent(matches[1]) : undefined;
+		}
+
+    	if (getCookie('source-mode') == 'development') {
+    		return true;
+    	} else {
+    		return false;
+    	}
+
+    }
+
     return new Utils();
 });
