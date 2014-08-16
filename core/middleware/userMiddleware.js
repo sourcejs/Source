bcb352d6400c38f63f3e8f4b9e9827fc40305953
Alexey Ostrovsky <ostroffsky@gmail.com>
2014-08-16 18:10:54 +0300

    ZAEBIS'

--- core/middleware/userMiddleware.js
+++ core/middleware/userMiddleware.js
@@ -6,15 +6,13 @@
 
 var fs = require('fs');
 var path = require('path');
-var pathToUserMiddleware = path.join(global.app.get('user'), 'core/middleware');
+var pathToUserMiddleware = path.join(global.app.get('user'), 'node_modules');
 var userMiddlewareFiles = fs.readdirSync( pathToUserMiddleware );
 
 // Loading all sourcejs-*/core/index.js files from npm plugins section
-console.log("LOAD");
 userMiddlewareFiles.map(function(file){
     if ((/^sourcejs-/).test(file)) {
-        var pluginIndexPath = path.join(pathToUserMiddleware, file, 'index.js');
-
+        var pluginIndexPath = path.join(pathToUserMiddleware, file, 'core/middleware/', file, 'index.js');
         if(fs.existsSync(pluginIndexPath)) {
             var middleware = require(pluginIndexPath);
             global.app.use(middleware.process);
