97ab53944db06cef0665305288c43150035d0af4
Alexey Ostrovsky <ostroffsky@gmail.com>
2014-08-27 13:22:06 +0400

    "user/node_modules" exists check

--- core/middleware/userMiddleware.js
+++ core/middleware/userMiddleware.js
@@ -7,15 +7,18 @@
 var fs = require('fs');
 var path = require('path');
 var pathToUserMiddleware = path.join(global.app.get('user'), 'node_modules');
-var userMiddlewareFiles = fs.readdirSync( pathToUserMiddleware );
 
 // Loading all sourcejs-*/core/index.js files from npm plugins section
-userMiddlewareFiles.map(function(file){
-    if ((/^sourcejs-/).test(file)) {
-        var pluginIndexPath = path.join(pathToUserMiddleware, file, 'core/middleware', file, 'index.js');
-        if(fs.existsSync(pluginIndexPath)) {
-            var middleware = require(pluginIndexPath);
-            global.app.use(middleware.process);
+if (fs.existsSync(pathToUserMiddleware)) {
+    var userMiddlewareFiles = fs.readdirSync(pathToUserMiddleware);
+
+    userMiddlewareFiles.map(function (file) {
+        if ((/^sourcejs-/).test(file)) {
+            var pluginIndexPath = path.join(pathToUserMiddleware, file, 'core/middleware', file, 'index.js');
+            if (fs.existsSync(pluginIndexPath)) {
+                var middleware = require(pluginIndexPath);
+                global.app.use(middleware.process);
+            }
         }
-    }
-});
+    });
+}
