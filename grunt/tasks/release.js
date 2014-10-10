1c2877b0fc20fd0b391f86a4f6fa3865ce41e55a
Ilya Mikhailov <ilya.mikhailov@hotmail.com>
2014-10-10 18:26:15 +0400

    config definition is improved; some comments are added

--- grunt/tasks/release.js
+++ grunt/tasks/release.js
@@ -6,28 +6,31 @@ module.exports = function (grunt) {
 
 	grunt.registerTask("release", "SourceJS release task", function() {
 		var envName = grunt.option('env');
-		var pkg = grunt.file.readJSON('package.json');
-
 		var options = this.options({
 			"configsPath": "configs",
 			"workspace": ".",
+			"ignores": [],
 			"keepReleases": 5,
-			"repositoryUrl": pkg.repository.url
+			"repositoryUrl": grunt.file.readJSON('package.json').repository.url,
+			"hooks": []
 		});
 
-		var config = formatShipitConfig(envName, options);
-		grunt.config.set('shipit', config);
-		createHooks();
-
+		grunt.config.set('shipit', getNormalizedConfig(envName, options));
+		createReleaseHooks(options.hooks);
 		grunt.task.run(["shipit:" + envName, "deploy"]);
 	});
 
-	var createHooks = function() {
+	// TODO: implement rollback task (m.b. using map + proxy)
+	grunt.registerTask("release", "SourceJS release task", function() {
+		throw new Error("NotImplementedMethod");
+	});
+
+	// TODO: add customizable hooks into task configuration (if needed)
+	var createHooks = function(hooks) {
 		grunt.registerTask('remote:restart', function () {
 			grunt.shipit.remote([
 					'cd ' + path.join(grunt.shipit.config.deployTo, 'current'),
 					'npm i',
-					'echo "test"'
 				].join(' && '),
 				this.async()
 			);
@@ -39,25 +42,35 @@ module.exports = function (grunt) {
 		});		
 	};
 
-	var formatShipitConfig = function(envName, options) {
-		var config = {};
+	// TODO: check if I shoud add flags redefinition which gonna have the highest priority
+	var getNormalizedConfig = function(envName, options) {
 		var envConfigPath = path.join(options.configsPath, envName + '.json');
 		if (!fs.existsSync(envConfigPath)) {
 			throw new Error("FileNotFoundError", "Config file " + envConfigPath + " not found");
 		}
 		var envConfig = grunt.file.readJSON(envConfigPath);
-		config.options = {
-                workspace: options.workspace || ".",
-                ignores: envConfig.ignores || [],
-                keepReleases: options.keepReleases,
-                repositoryUrl: options.repositoryUrl,
-                servers: envConfig.servers
-            };
-        config[envName] = {
-        	"branch": envConfig.branch,
-        	"deployTo": envConfig.deployTo
-        };
+		var config = {
+			"options": getNormalizedOptions(envConfig, options)
+		};
+		config[envName] = getNormalizedEnvOptions(envConfig, options);
 		return config;
+	};
+
+	var getNormalizedOptions = function(envOpts, taskOpts) {
+		return {
+			"workspace": envOpts.workspace || taskOpts.workspace,
+            "ignores": envConfig.ignores || taskOpts.ignores,
+            "keepReleases": envConfig.keepReleases || options.keepReleases,
+            "repositoryUrl": envConfig.keepReleases || options.repositoryUrl,
+            "servers": envConfig.servers || options.servers
+		};
+	};
+
+	var getNormalizedEnvOptions = function(envOpts, taskOpts) {
+		return {
+        	"branch": envConfig.branch || taskOpts.branch,
+        	"deployTo": envConfig.deployTo || taskOpts.deployTo
+        }
 	};
 	
 };
