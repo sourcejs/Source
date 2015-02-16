f3a4a80fc09be345402cab4deb3ba98ceb3aa27c
Ilya Mikhailov <ilya.mikhailov@hotmail.com>
2015-02-16 16:25:30 +0300

    some polishing, auth description draft is added

--- assets/js/enter-the-source.js
+++ assets/js/enter-the-source.js
@@ -13,7 +13,7 @@ require([
     ], function ($, options, browser, Loader, Auth) {
         if (options && options.modulesEnabled && options.modulesEnabled.auth === true) {
             new Auth({
-                target: $('.js-hook')
+                target: $('.js-hook.source_login')
             });
         }
 });
