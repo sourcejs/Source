2547fbecd8ee006d6f3a1429a443960c816261af
Robert Haritonov <r@rhr.me>
2015-03-28 16:54:55 +0100

    Removing prism line numbers

--- assets/js/lib/prism/prism.js
+++ assets/js/lib/prism/prism.js
@@ -502,28 +502,3 @@ Prism.languages.insertBefore('bash', 'comment', {
     //shebang must be before comment, 'important' class from css reused
     'important': /(^#!\s*\/bin\/bash)|(^#!\s*\/bin\/sh)/g
 });
-;
-Prism.hooks.add('after-highlight', function (env) {
-    // works only for <code> wrapped inside <pre data-line-numbers> (not inline)
-    var pre = env.element.parentNode;
-
-    if (!pre || !/pre/i.test(pre.nodeName)) {
-        return;
-    }
-    var linesNum = (1 + env.code.split('\n').length);
-    var lineNumbersWrapper;
-
-    lines = new Array(linesNum);
-    lines = lines.join('<span></span>');
-
-    lineNumbersWrapper = document.createElement('span');
-    lineNumbersWrapper.className = 'line-numbers-rows';
-    lineNumbersWrapper.innerHTML = lines;
-
-    if (pre.hasAttribute('data-start')) {
-        pre.style.counterReset = 'linenumber ' + (parseInt(pre.getAttribute('data-start'), 10) - 1);
-    }
-
-    env.element.appendChild(lineNumbersWrapper);
-
-});
