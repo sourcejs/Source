8d684015b8e96ed8aeae04304ec26d89b8a85cea
Robert Haritonov <r@rhr.me>
2015-05-27 23:34:11 +0200

    use path.join in headerFooter.js

--- core/headerFooter.js
+++ core/headerFooter.js
@@ -2,20 +2,21 @@
 
 var fs = require('fs');
 var ejs = require('ejs');
+var path = require('path');
 
 exports.getHeaderAndFooter = function () {
-    var defaultTemplatePath = global.pathToApp + '/assets/templates/';
-    var userTemplatePath = global.app.get('user') + '/assets/templates/';
+    var defaultTemplatePath = path.join(global.pathToApp, 'assets/templates');
+    var userTemplatePath = path.join(global.app.get('user'), 'assets/templates');
     var headerFile = 'header.inc.html';
     var footerFile = 'footer.inc.html';
 
     var output = {};
 
-    var userHeaderTplPath = userTemplatePath + headerFile;
-    var headerTplPath = fs.existsSync(userHeaderTplPath) ? userHeaderTplPath : defaultTemplatePath + headerFile;
+    var userHeaderTplPath = path.join(userTemplatePath, headerFile);
+    var headerTplPath = fs.existsSync(userHeaderTplPath) ? userHeaderTplPath : path.join(defaultTemplatePath, headerFile);
 
-    var userFooterTplPath = userTemplatePath + footerFile;
-    var footerTplPath = fs.existsSync(userFooterTplPath) ? userFooterTplPath : defaultTemplatePath + footerFile;
+    var userFooterTplPath = path.join(userTemplatePath, footerFile);
+    var footerTplPath = fs.existsSync(userFooterTplPath) ? userFooterTplPath : path.join(defaultTemplatePath, footerFile);
 
     output.header = ejs.render(fs.readFileSync(headerTplPath, 'utf-8'));
     output.footer = ejs.render(fs.readFileSync(footerTplPath, 'utf-8'), {
