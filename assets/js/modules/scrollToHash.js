120b8bc2fd2584f11e82ffd299c3a6d967f0109c
Robert Haritonov <r@rhr.me>
2015-06-11 13:04:48 +0200

    wrap strollToHash with DOM ready

--- assets/js/modules/scrollToHash.js
+++ assets/js/modules/scrollToHash.js
@@ -8,42 +8,43 @@ define([
 
     'use strict';
 
-    var navHash = utils.parseNavHash();
-    var $sections = $('.source_section');
-    var $navigation = $('.source_nav');
-    var $mainContainer = $('.source_main');
-    var $body = $('body');
+    $(function(){
+        var navHash = utils.parseNavHash();
+        var $sections = $('.source_section');
+        var $navigation = $('.source_nav');
+        var $mainContainer = $('.source_main');
+        var $body = $('body');
 
-    // Show hidden sections and navigation
-    function showSections() {
-        $sections.addClass('__loaded');
-        $navigation.addClass('__loaded');
-        $mainContainer.removeClass('__loading');
-        $body.removeClass('__loading');
-    }
-
-    $mainContainer.addClass('__loading');
-    $body.addClass('__loading');
-
-    loadEvents.init(function() {
-        showSections();
-
-        // FF scroll bug, related native to scroll to hash conflicts
-        if ($('html').hasClass('mozilla')) {
-            var triesCount = 0;
-
-            var t = setInterval(function() {
-                var scrollTopCord = utils.scrollToSection(navHash);
-
-                if (window.pageYOffset === scrollTopCord || triesCount < 4) {
-                    clearInterval(t);
-                } else {
-                    triesCount++;
-                }
-            }, 300);
-        } else {
-            utils.scrollToSection(navHash);
+        // Show hidden sections and navigation
+        function showSections() {
+            $sections.addClass('__loaded');
+            $navigation.addClass('__loaded');
+            $mainContainer.removeClass('__loading');
+            $body.removeClass('__loading');
         }
-    });
 
+        $mainContainer.addClass('__loading');
+        $body.addClass('__loading');
+
+        loadEvents.init(function() {
+            showSections();
+
+            // FF scroll bug, related native to scroll to hash conflicts
+            if ($('html').hasClass('mozilla')) {
+                var triesCount = 0;
+
+                var t = setInterval(function() {
+                    var scrollTopCord = utils.scrollToSection(navHash);
+
+                    if (window.pageYOffset === scrollTopCord || triesCount < 4) {
+                        clearInterval(t);
+                    } else {
+                        triesCount++;
+                    }
+                }, 300);
+            } else {
+                utils.scrollToSection(navHash);
+            }
+        });
+    });
 });
