9447c7522e7d334ce700ef2d804eb5f86cabfb7e
Robert Haritonov <r@rhr.me>
2015-10-04 17:46:58 +0200

    fix linking user folder when sourcejs in npm linked

--- core/postInstall.js
+++ core/postInstall.js
@@ -1,21 +1,21 @@
 'use strict';
 
 var path = require('path');
+var fs = require('fs-extra');
 var link = require('./lib/createLink');
 
-var pathToApp = __dirname.replace(/^\w:\\/, function (match) {
+var pathToApp = process.env.PWD.replace(/^\w:\\/, function (match) {
     return match.toLowerCase();
 });
 
-var enginePath = path.join(pathToApp, '../');
-var userPath = path.join(enginePath, '../../');
-var internalUserPath = path.join(enginePath, 'user');
-var parentNodeModules = path.join(enginePath, '../');
+var userPath = path.join(pathToApp, '../../');
+
+var internalUserPath = path.join(pathToApp, 'user');
+var parentNodeModules = path.join(pathToApp, '../');
 
 // check if sourcejs is installed as a node_package
-if (
-    path.relative(parentNodeModules, enginePath) === 'sourcejs'
-) {
+if (path.relative(parentNodeModules, pathToApp) === 'sourcejs') {
+    fs.removeSync(internalUserPath);
     link(userPath, internalUserPath, 'dir');
     console.log('SourceJS User folder symlink created.');
 }
