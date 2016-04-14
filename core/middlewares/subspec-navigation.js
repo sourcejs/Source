ec6319389a8630d7978b4f6e6c164a88db970fc2
Norbert de Langen <norbert@macaw.nl>
2016-04-14 23:33:09 +0200

    ADD grabbing spec sections from renderedHTML

--- core/middlewares/subspec-navigation.js
+++ core/middlewares/subspec-navigation.js
@@ -3,6 +3,7 @@
 var fs = require('fs-extra');
 var path = require('path');
 var _ = require('lodash');
+var cheerio = require('cheerio');
 
 var ejs = require(path.join(global.pathToApp, 'core/ejsWithHelpers.js'));
 var viewResolver = require(path.join(global.pathToApp + '/core/lib/viewResolver.js'));
@@ -21,7 +22,16 @@ exports.process = function(req, res, next) {
 
     // Check if spec is request and ejs pre-rendering enabled
     if (req.specData && req.specData.info && req.specData.info.role !== 'navigation') {
-        global.console.log(req.specData);
+
+				var data = req.specData.renderedHtml.replace(/^\s+|\s+$/g, '');
+				var $ = cheerio.load(data, { decodeEntities: false });
+
+				var $sections = _.map($('.' + req.specData.contextOptions.assets.SECTION_CLASS), function (section) {
+					return section.attribs.id;
+				});
+
+				global.console.log($sections);
+
         req.placeholders.sidecontent = _.concat(req.placeholders.sidecontent, 'subspec-navigation.js: Hello world!');
         next();
     } else {
