d68487d38a89d471e7792c0739a070b03c0d8c48
Norbert de Langen <norbert@macaw.nl>
2016-04-19 21:47:56 +0200

    CHANGED serverside sections data into a new middleware

--- core/middlewares/sections.js
+++ core/middlewares/sections.js
@@ -10,7 +10,7 @@ var viewResolver = require(path.join(global.pathToApp + '/core/lib/viewResolver.
 var specUtils = require(path.join(global.pathToApp,'core/lib/specUtils'));
 
 /**
-* Processing pre-ejs rendering on spec request
+* Enriching specData with sections
 *
 * @param {object} req - Request object
 * @param {object} res - Response object
@@ -19,12 +19,20 @@ var specUtils = require(path.join(global.pathToApp,'core/lib/specUtils'));
 exports.process = function(req, res, next) {
     // Check if spec is request
     if (req.specData && req.specData.info && req.specData.info.role !== 'navigation') {
-        req.placeholders = req.placeholders || {};
-        req.placeholders.sidecontent = req.placeholders.sidecontent || [];
+        var data = req.specData.renderedHtml.replace(/^\s+|\s+$/g, '');
+        var $ = cheerio.load(data, { decodeEntities: false });
 
-        console.log(req.specData.sections);
+        req.specData.sections = _.map($('.' + req.specData.contextOptions.assets.SECTION_CLASS), function (item, index) {
+            var section = $(item);
+            var section_title = section.children().first().text();
 
-        req.placeholders.sidecontent = _.concat(req.placeholders.sidecontent, 'subspec-navigation.js: Hello world!');
+            return {
+              id: index + section_title.replace(/[^a-z0-9]/gi, '').toLowerCase(),
+              title: section_title,
+              children: [] // TODO
+            };
+        });
+
         next();
     } else {
         next();
