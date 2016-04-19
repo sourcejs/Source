a1e095758ad36f3d460b0b9be8c03ffd3ef10a57
Norbert de Langen <norbert@macaw.nl>
2016-04-19 22:38:04 +0200

    CHANGE the parsing of specHTML for sections to existing lib. CLEANUP of dependencies

--- core/middlewares/sections.js
+++ core/middlewares/sections.js
@@ -1,13 +1,11 @@
 'use strict';
 
-var fs = require('fs-extra');
 var path = require('path');
-var _ = require('lodash');
-var cheerio = require('cheerio');
 
-var ejs = require(path.join(global.pathToApp, 'core/ejsWithHelpers.js'));
-var viewResolver = require(path.join(global.pathToApp + '/core/lib/viewResolver.js'));
-var specUtils = require(path.join(global.pathToApp,'core/lib/specUtils'));
+var pathToApp = path.dirname(require.main.filename);
+var specsParser = require(path.join(pathToApp, 'core/lib/specPageParser'));
+
+
 
 /**
 * Enriching specData with sections
@@ -19,20 +17,7 @@ var specUtils = require(path.join(global.pathToApp,'core/lib/specUtils'));
 exports.process = function(req, res, next) {
     // Check if spec is request
     if (req.specData && req.specData.info && req.specData.info.role !== 'navigation') {
-        var data = req.specData.renderedHtml.replace(/^\s+|\s+$/g, '');
-        var $ = cheerio.load(data, { decodeEntities: false });
-
-        req.specData.sections = _.map($('.' + req.specData.contextOptions.assets.SECTION_CLASS), function (item, index) {
-            var section = $(item);
-            var section_title = section.children().first().text();
-
-            return {
-              id: index + section_title.replace(/[^a-z0-9]/gi, '').toLowerCase(),
-              title: section_title,
-              children: [] // TODO
-            };
-        });
-
+        req.specData.sections = specsParser.process(req.specData.renderedHtml).contents;
         next();
     } else {
         next();
